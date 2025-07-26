ed31391bf698473903ffca935d1d85204f98eecd
Allan8606 <allan-musico@hotmail.com>
2025-07-25 22:22:08 -0300

    Criando a classe/tabela missoes, criando um relacionamento e criando chave estrangeira

--- Spring/CadastroDeNinjas/src/main/java/br/com/allanisaacdev/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ Spring/CadastroDeNinjas/src/main/java/br/com/allanisaacdev/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,7 +1,10 @@
-package br.com.allanisaacdev.CadastroDeNinjas;
+package br.com.allanisaacdev.CadastroDeNinjas.Ninjas;
 
 
+import br.com.allanisaacdev.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro_de_ninjas")
@@ -11,8 +14,17 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+
     private String email;
+
     private  int idade;
+
+    // Um ninja tem apenas uma unica missão.
+    @ManyToMany
+    @JoinColumn(name = "missoes_id")//Chave estrangeira. Esse nome fomos nós que escolhemos.
+    private MissoesModel missoes;
+
+
 
     public NinjaModel() {
     }
