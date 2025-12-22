26e55ff3983c682e3cf65ff9f4bacb2c9f63a953
Allan860 <allan-musico@hotmail.com>
2025-12-22 15:43:57 -0300

    feat: Cria a classe SecurityFilterChain para as configurações do spring security, mas ainda não implementa os métodos

---
+++ 05 - Spring Intermediario/MovieFlix/src/main/java/com/allan/dev/MovieFlix/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.allan.dev.MovieFlix.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // private final SecurityFilter securityFilter;
+
+    //Cadeia de Filtros de Segurança
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+    }
+
+ /*   @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+  */
+
+
+}
